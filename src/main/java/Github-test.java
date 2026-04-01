f0319843a9b5aacea0f721522c392933a9dbe6c2
kyonfunIto-test <kyonfun.kensho@gmail.com>
2026-04-01 19:11:18 +0900

    Update Github-test.java

---
+++ src/main/java/Github-test.java
@@ -0,0 +1,9 @@
+public class SecretScanTest {
+    // Slack Bot Token 風
+    private static final String SLACK_BOT_TOKEN = "xoxb-123456789018-123456789013-abcdefghijklmnopqrstuvwxyz12";
+ 
+    public static void main(String[] args) {
+        System.out.println("Secret scan test");
+    }
+}
+
